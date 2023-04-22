f04bbb173e3c8aada28444dc4f300bdc14e2cfd6
寒寒 <2596194220@qq.com>
2023-04-22 21:29:04 +0800

    定义DockerFile以及cl/cd

---
+++ start.sh
@@ -0,0 +1,21 @@
+# 强制删除运行的容器
+docker rm -f hanhan-note
+
+# 休息一秒钟
+sleep 1s
+
+# 强制删除镜像
+docker rmi -f hanhan-note
+
+# 构建镜像
+docker build -t hanhan-note .
+
+# 运行镜像
+docker run -itd -p 5001:80 --name hanhan-note hanhan-note
+
+# 删除文件
+rm -rf .
+
+
+
+
